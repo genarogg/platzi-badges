bab4a55c4c6d76882cd1814d2793431f889d3c68
genarogg <gena_gonzalez15@hotmail.com>
2019-10-02 23:45:41 -0400

    useMemo

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -30,7 +30,15 @@ function useSearchBadges(badges) {
   const [query, setQuery] = React.useState('');
   const [filteredBadges, setFilteredBadges] = React.useState(badges);
 
-  
+  React.useMemo(() => {
+    const result = badges.filter(badge => {
+      return `${badge.firstName} ${badge.lastName}`
+        .toLowerCase()
+        .includes(query.toLowerCase());
+    });
+
+    setFilteredBadges(result);
+  }, [badges, query]);
 
   return { query, setQuery, filteredBadges };
 }
