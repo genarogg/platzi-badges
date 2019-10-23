d965ee863d462f6605dc2d86ab2b3b99eccea77f
genarogg <gena_gonzalez15@hotmail.com>
2019-10-22 22:55:45 -0400

    BadgeNew.js

---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './styles/BadgeNew.css';
+import header from '../images/badge-header.svg';
+import Navbar from '../components/Navbar';
+import Badge from '../components/Badge';
+
+class BadgeNew extends React.Component {
+    render() {
+      return (
+        <div>
+          
+  
+          
+        </div>
+      );
+    }
+  }
+  
+  export default BadgeNew;
+  
