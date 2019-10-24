566b3d656bfd1ac610e5435ac7440ad03ff81db9
genarogg <gena_gonzalez15@hotmail.com>
2019-10-23 23:00:34 -0400

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles/Navbar.css';
+import logo from '../images/logo.svg';
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default Navbar;
