c366b4686e471fbb9e08abfecf287f00c5d9e8b0
genarogg <gena_gonzalez15@hotmail.com>
2019-10-24 11:29:35 -0400

    handleChange

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -17,7 +17,14 @@ class BadgeNew extends React.Component {
     },
   };
 
-  
+  handleChange = e => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
 
   render() {
     return (
