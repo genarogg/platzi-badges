18aef65bb98e1b9ee0667b017e517b72cb265530
genarogg <gena_gonzalez15@hotmail.com>
2019-10-24 11:29:20 -0400

    reestructuracion de badgeNew: state

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -7,31 +7,24 @@ import BadgeForm from '../components/BadgeForm';
 import Navbar from '../components/Navbar';
 
 class BadgeNew extends React.Component {
+  state = {
+    form: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      jobTitle: '',
+      twitter: '',
+    },
+  };
+
+  
+
   render() {
     return (
       <div>
-        <Navbar />
-        <div className="BadgeNew__hero">
-          <img className="img-fluid" src={header} alt="Logo" />
-        </div>
+        
 
-        <div className="container">
-          <div className="row">
-            <div className="col-6">
-              <Badge
-                firstName="Richard"
-                lastName="Kaufman"
-                twitter="sparragus"
-                jobTitle="Frontend Engineer"
-                avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
-              />
-            </div>
-
-            <div className="col-6">
-              <BadgeForm />
-            </div>
-          </div>
-        </div>
+        
       </div>
     );
   }
