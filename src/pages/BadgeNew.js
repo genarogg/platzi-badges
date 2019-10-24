c5fc5ca4640c8241158bb4a37484f6154bbaf280
genarogg <gena_gonzalez15@hotmail.com>
2019-10-24 13:18:16 -0400

    <Badge />

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -37,7 +37,14 @@ class BadgeNew extends React.Component {
         <div className="container">
           <div className="row">
             <div className="col-6">
-              
+              <Badge
+                firstName={this.state.form.firstName}
+                lastName={this.state.form.lastName}
+                twitter={this.state.form.twitter}
+                jobTitle={this.state.form.jobTitle}
+                email={this.state.form.email}
+                avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
+              />
             </div>
 
             <div className="col-6">
