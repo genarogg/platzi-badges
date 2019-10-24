f23e8f2e70e65ce0dd1a90e6ba3d6635eeda712b
genarogg <gena_gonzalez15@hotmail.com>
2019-10-24 11:26:22 -0400

    <div className="Badges">

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -13,7 +13,19 @@ class Badges extends React.Component {
   render() {
     return (
       <div>
-        
+        <Navbar />
+
+        <div className="Badges">
+          <div className="Badges__hero">
+            <div className="Badges__container">
+              <img
+                className="Badges_conf-logo"
+                src={confLogo}
+                alt="Conf Logo"
+              />
+            </div>
+          </div>
+        </div>
 
         <div className="Badges__container">
           <div className="Badges__buttons">
