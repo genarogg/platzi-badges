519ad7dff2c3e951725a8296946ee2187413b887
genarogg <gena_gonzalez15@hotmail.com>
2019-10-24 00:52:28 -0400

    handleChange

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 class BadgeForm extends React.Component {
-  
+  handleChange = e => {
+    console.log({
+      name: e.target.name,
+      value: e.target.value,
+    });
+  };
 
   
 
