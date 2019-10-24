df8b15bf063e27f8253bf65b61ef4a12671981f8
genarogg <gena_gonzalez15@hotmail.com>
2019-10-24 11:24:59 -0400

    class Badges extends React.Component {

---
+++ src/pages/Badges.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './styles/Badges.css';
+import confLogo from '../images/badge-header.svg';
+import Navbar from '../components/Navbar';
+import BadgesList from '../components/BadgesList';
+
+class Badges extends React.Component {
+  state = {
+    
+  };
+
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default Badges;
