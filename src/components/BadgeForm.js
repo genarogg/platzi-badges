cbe21ad4bcfd34b6c377caad6e9b6348bb847b18
genarogg <gena_gonzalez15@hotmail.com>
2019-10-24 00:54:08 -0400

    formaulario de badgeForm

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -12,14 +12,32 @@ class BadgeForm extends React.Component {
     console.log('Button was clicked');
   };
 
-   = e => {
+  handleSubmit = e => {
     e.preventDefault();
     console.log('Form was submitted');
   };
 
   render() {
     return (
-      
+      <div>
+        <h1>New Attendant</h1>
+
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label>First Name</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="firstName"
+            />
+          </div>
+
+          <button onClick={this.handleClick} className="btn btn-primary">
+            Save
+          </button>
+        </form>
+      </div>
     );
   }
 }
