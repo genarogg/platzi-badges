5bcee994c0ae3f7b9899f3d0dada2b29ac417747
genarogg <gena_gonzalez15@hotmail.com>
2019-10-25 13:45:51 -0400

    Ajustes para index(rutas) y en badges.js(de etiquetas a a etiquetas to)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap4/dist/css/bootstrap.css';
 
 import './global.css';
-import BadgeNew from './pages/BadgeNew';
-import Badges from './pages/Badges';
+import App from './components/App';
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<Badges />, container);
+ReactDOM.render(<App />, container);
+
