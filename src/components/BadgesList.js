6f2b4d2a2b2ff058f1f21f6a38aaff7f74509ca9
genarogg <gena_gonzalez15@hotmail.com>
2019-10-25 13:27:54 -0400

    <div className="BadgesListItem">

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -5,7 +5,15 @@ import './styles/BadgesList.css';
 class BadgesListItem extends React.Component {
   render() {
     return (
-      
+      <div className="BadgesListItem">
+        <img
+          className="BadgesListItem__avatar"
+          src={this.props.badge.avatarUrl}
+          alt={`${this.props.badge.firstName} ${this.props.badge.lastName}`}
+        />
+
+        
+      </div>
     );
   }
 }
