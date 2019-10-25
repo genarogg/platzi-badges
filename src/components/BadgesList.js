7712d059b1a8237fa8625681b61d1962398e7f48
genarogg <gena_gonzalez15@hotmail.com>
2019-10-25 13:29:01 -0400

    <div className="BadgesList">

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -28,7 +28,17 @@ class BadgesListItem extends React.Component {
 class BadgesList extends React.Component {
   render() {
     return (
-      
+      <div className="BadgesList">
+        <ul className="list-unstyled">
+          {this.props.badges.map(badge => {
+            return (
+              <li key={badge.id}>
+                <BadgesListItem badge={badge} />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     );
   }
 }
