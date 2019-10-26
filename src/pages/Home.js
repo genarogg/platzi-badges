922516e23d6d1876cb1a30bf4c720b762d9f72a7
genarogg <gena_gonzalez15@hotmail.com>
2019-10-26 14:16:22 -0400

    home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import './styles/Home.css';
+import platziconfLogoImage from '../images/platziconf-logo.svg';
+import astronautsImage from '../images/astronauts.svg';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="container">
+          <div className="row">
+            
+
+            
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
