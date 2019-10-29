5b4f140d2db0e2dd58afdb72a78c5ac0b5ac6278
genarogg <gena_gonzalez15@hotmail.com>
2019-10-28 21:11:58 -0400

    <Badge />

--- src/pages/BadgeEdit.js
+++ src/pages/BadgeEdit.js
@@ -33,7 +33,14 @@ class BadgeEdit extends React.Component {
         <div className="container">
           <div className="row">
             <div className="col-6">
-              
+              <Badge
+                firstName={this.state.form.firstName || 'FIRST_NAME'}
+                lastName={this.state.form.lastName || 'LAST_NAME'}
+                twitter={this.state.form.twitter || 'twitter'}
+                jobTitle={this.state.form.jobTitle || 'JOB_TITLE'}
+                email={this.state.form.email || 'EMAIL'}
+                avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
+              />
             </div>
 
             <div className="col-6">
