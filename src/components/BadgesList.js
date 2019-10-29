00223003dd81984d20cc3daa02b83f8f7572a002
genarogg <gena_gonzalez15@hotmail.com>
2019-10-28 21:27:15 -0400

    return para para bade edit

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './styles/BadgesList.css';
+import Gravatar from './Gravatar';
 
 class BadgesListItem extends React.Component {
   render() {
     return (
       <div className="BadgesListItem">
-        <img
+        <Gravatar
           className="BadgesListItem__avatar"
-          src={this.props.badge.avatarUrl}
-          alt={`${this.props.badge.firstName} ${this.props.badge.lastName}`}
+          email={this.props.badge.email}
         />
 
         <div>
@@ -45,7 +45,12 @@ class BadgesList extends React.Component {
           {this.props.badges.map(badge => {
             return (
               <li key={badge.id}>
-                <BadgesListItem badge={badge} />
+                <Link
+                  className="text-reset text-decoration-none"
+                  to={`/badges/${badge.id}/edit`}
+                >
+                  <BadgesListItem badge={badge} />
+                </Link>
               </li>
             );
           })}
