cb7624c17bd79ebd86feb300ffb8640b4d0555e3
genarogg <gena_gonzalez15@hotmail.com>
2019-10-29 22:36:44 -0400

    <div className="BadgeDetails__hero">

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -43,7 +43,20 @@ class BadgeDetails extends React.Component {
 
     return (
       <div>
-       
+        <div className="BadgeDetails__hero">
+          <div className="container">
+            <div className="row">
+              <div className="col-6">
+                <img src={confLogo} alt="Logo de la Conferencia" />
+              </div>
+              <div className="col-6 BadgeDetails__hero-attendant-name">
+                <h1>
+                  {badge.firstName} {badge.lastName}
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
 
         
       </div>
