8eb3b80d9a7d6eea3714babc285726a11d42520a
genarogg <gena_gonzalez15@hotmail.com>
2019-10-29 22:33:51 -0400

    BadgeDetails

---
+++ src/pages/BadgeDetails.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './styles/BadgeDetails.css';
+import confLogo from '../images/platziconf-logo.svg';
+import PageLoading from '../components/PageLoading';
+import PageError from '../components/PageError';
+import Badge from '../components/Badge';
+import api from '../api';
+
+class BadgeDetails extends React.Component {
+  
+
+ 
+
+  
+
+  render() {
+    
+
+    return (
+      <div>
+       
+
+        
+      </div>
+    );
+  }
+}
+
+export default BadgeDetails;
