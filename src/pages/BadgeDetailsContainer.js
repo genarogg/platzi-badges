d044b0aaf8f26b5439f058b10b3d2caa8e18fb29
genarogg <gena_gonzalez15@hotmail.com>
2019-10-29 22:45:18 -0400

    BadgeDetailsContainer

---
+++ src/pages/BadgeDetailsContainer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import BadgeDetails from './BadgeDetails';
+import PageLoading from '../components/PageLoading';
+import PageError from '../components/PageError';
+import api from '../api';
+
+class BadgeDetailsContainer extends React.Component {
+  state = {
+    loading: true,
+    error: null,
+    data: undefined,
+  };
+
+ 
+
+  
+
+ 
+}
+
+export default BadgeDetailsContainer;
