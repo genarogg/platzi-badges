1aec471897bd506b6a8e63b8059248b91c2f2d48
genarogg <gena_gonzalez15@hotmail.com>
2019-10-31 23:16:14 -0400

    DeleteBadgeModal

---
+++ src/components/DeleteBadgeModal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Modal from './Modal';
+
+function DeleteBadgeModal(props) {
+  return (
+    <Modal isOpen={props.isOpen} onClose={props.onClose}>
+      
+    </Modal>
+  );
+}
+
+export default DeleteBadgeModal;
