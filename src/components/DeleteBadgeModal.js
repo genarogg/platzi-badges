81060d858e1dc39562c0d7be218e404e0a6848ca
genarogg <gena_gonzalez15@hotmail.com>
2019-10-31 23:16:27 -0400

    <div className="DeleteBadgeModal">

--- src/components/DeleteBadgeModal.js
+++ src/components/DeleteBadgeModal.js
@@ -5,7 +5,12 @@ import Modal from './Modal';
 function DeleteBadgeModal(props) {
   return (
     <Modal isOpen={props.isOpen} onClose={props.onClose}>
-      
+      <div className="DeleteBadgeModal">
+        <h1>Are You Sure?</h1>
+        <p>You are about to delete this badge.</p>
+
+        
+      </div>
     </Modal>
   );
 }
