a221749a6557e81cef23c1a84ca783b5f209502f
genarogg <gena_gonzalez15@hotmail.com>
2019-10-31 23:17:11 -0400

    DeleteBadgeModal.js

--- src/components/DeleteBadgeModal.js
+++ src/components/DeleteBadgeModal.js
@@ -9,7 +9,14 @@ function DeleteBadgeModal(props) {
         <h1>Are You Sure?</h1>
         <p>You are about to delete this badge.</p>
 
-        
+        <div>
+          <button onClick={props.onDeleteBadge} className="btn btn-danger mr-4">
+            Delete
+          </button>
+          <button onClick={props.onClose} className="btn btn-primary">
+            Cancel
+          </button>
+        </div>
       </div>
     </Modal>
   );
