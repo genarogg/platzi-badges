cf52fe2585fd368a187e99bc424f5516b9a14756
genarogg <gena_gonzalez15@hotmail.com>
2019-10-31 23:12:27 -0400

    handleDeleteBadge

--- src/pages/BadgeDetailsContainer.js
+++ src/pages/BadgeDetailsContainer.js
@@ -34,7 +34,18 @@ class BadgeDetailsContainer extends React.Component {
     this.setState({ modalIsOpen: false });
   };
 
-  
+  handleDeleteBadge = async e => {
+    this.setState({ loading: true, error: null });
+
+    try {
+      await api.badges.remove(this.props.match.params.badgeId);
+      this.setState({ loading: false });
+
+      this.props.history.push('/badges');
+    } catch (error) {
+      this.setState({ loading: false, error: error });
+    }
+  };
 
   render() {
     if (this.state.loading) {
